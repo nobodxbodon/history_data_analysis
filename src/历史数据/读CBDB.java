852d80fc17fe52a7fb3216b2a5a64110c9fb9713
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-04-22 22:59:12 -0700

    大姓

--- src/历史数据/读CBDB.java
+++ src/历史数据/读CBDB.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 // 参考: https://www.sqlitetutorial.net/sqlite-java/
 public class 读CBDB {
@@ -20,6 +24,37 @@ public class 读CBDB {
     return 联结;
   }
 
+  public void 统计姓() {
+    String sql = "SELECT c_surname_chn FROM biog_main";
+    Map<String, Integer> unSortedMap = new HashMap<>();
+
+    try {
+      Connection 联结 = this.连接();
+      Statement stmt = 联结.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while (rs.next()) {
+        String 姓 = rs.getString("c_surname_chn");
+
+        if (unSortedMap.containsKey(姓)) {
+          unSortedMap.put(姓, unSortedMap.get(姓) + 1);
+        } else {
+          unSortedMap.put(姓, 1);
+        }
+      }
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // 排序参考: https://howtodoinjava.com/sort/java-sort-map-by-values/
+    LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+
+    unSortedMap.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+
+    System.out.println("大姓: " + reverseSortedMap);
+  }
+
   public void 所有人() {
     String sql = "SELECT c_name_chn, c_surname_chn FROM biog_main";
 
@@ -30,17 +65,16 @@ public class 读CBDB {
 
       int 人数 = 0;
       int 问题 = 0;
-      // loop through the result set
       while (rs.next()) {
         String 全名 = rs.getString("c_name_chn");
         String 姓 = rs.getString("c_surname_chn");
-        
+
         if (人数 < 10) {
           System.out.println(全名 + "\t" + 姓);
         } else {
-          if (姓 != null && (全名 == null || 全名.isEmpty() || 姓.isEmpty() || 全名.indexOf(姓)!= 0)) {
+          if (姓 != null && (全名 == null || 全名.isEmpty() || 姓.isEmpty() || 全名.indexOf(姓) != 0)) {
             System.out.println("? " + 全名 + "\t" + 姓);
-            问题 ++;
+            问题++;
           }
         }
         人数++;
@@ -53,6 +87,6 @@ public class 读CBDB {
 
   public static void main(String[] args) {
     读CBDB 读 = new 读CBDB();
-    读.所有人();
+    读.统计姓();
   }
 }
